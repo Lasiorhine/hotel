73776cada464b0e683998f46dafa3b79791e237a
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-03-05 20:33:54 -0800

    After consideration, price calculation methods added to reservation class. Tests written and failing in the desired way.

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -7,25 +7,29 @@ require 'pry'
 
 module Hotel
   class Reservation
-    attr_accessor :id, :start_date, :end_date
+    attr_accessor :start_date, :end_date
+    attr_reader :id, :total_nights, :total_reservation_cost
 
     @@last_id_base = 0
 
-    #36000 seconds is ten hours. This figure was arrived at by taking the most likely check-out time (10:00 am) and determining the number of seconds since the change of date at midnight. The putative customer could change this easily, depending on their needs.
+    # The constant below, MIN_RES_IN_SEC, is the length of the minimum reservation, in seconds.  This figure was arrived at by calculating the number of seconds between an early-but-common check-out time (10:00 am) and the change of date at midnight.  The putative customer could, of course, change this to whatever they wanted.
     MIN_RES_IN_SEC = 36000
+
+    #Since all rooms are identical, and we haven't been given any parameters for price variation, it makes sense to assign a default price for the moment.  If the customer needs more options or granularity here, that cna be implemented later
+    PER_NIGHT_PRICE = 100.00
 
     def initialize(start_date, end_date)
       @id = assign_id
       @start_date = DateTime.parse(start_date)
       @end_date = DateTime.parse(end_date)
+      @total_nights = calculate_total_nights
+      @total_reservation_cost = calculate_reservation_price
+
 
       if (@start_date.to_time.to_i - @end_date.to_time.to_i) > MIN_RES_IN_SEC || @start_date.to_time.to_i < Time.now.to_i
-        raise StandardError.new("A reservation's end date must come after its start date.")
+        raise StandardError.new("A reservation's end date must come after its start date, and it must be at least one night long.")
       end
     end
-
-    # def calculate_cumulative_price
-    # end
 
     def assign_id
       new_id_base = (@@last_id_base + 1).to_s
@@ -34,6 +38,14 @@ module Hotel
       leading_zero_array = (1..zeroes_needed).collect {"0"}
       new_id = leading_zero_array.join.concat(new_id_base)
       return new_id
+    end
+
+    def calculate_total_nights
+      (@start_date.to_date - @end_date.to_date).to_i
+    end
+
+    def calculate_reservation_price
+      (@total_nights * PER_NIGHT_PRICE).round(2)
     end
   end
 end
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -4,24 +4,24 @@ require_relative 'spec_helper'
 describe "Reservation class" do
 
   before do
-    @reservation_0_nominal = Hotel::Reservation.new('10th Jun 3013', '16th Jun 3013')
-    @reservation_1_id_check = Hotel::Reservation.new('1st Oct 3080', '4th Oct 3080')
-    @reservation_2_id_check = Hotel::Reservation.new('1st Nov 3081', '4th Nov 3081')
-    @reservation_3_id_check = Hotel::Reservation.new('1st Dec 3082', '5th Dec 3082')
+    @reservation_0_nominal_6n = Hotel::Reservation.new('10th Jun 3013', '16th Jun 3013')
+    @reservation_1_1n = Hotel::Reservation.new('1st Oct 3080', '2nd Oct 3080')
+    @reservation_2_2n = Hotel::Reservation.new('1st Nov 3081', '3rd Nov 3081')
+    @reservation_3_35n = Hotel::Reservation.new('1st Dec 3082', '5th Jan 3083')
   end
 
   describe "initialize" do
 
     it "is an instance of Reservation" do
-      @reservation_0_nominal.must_be_instance_of Hotel::Reservation
+      @reservation_0_nominal_6n.must_be_instance_of Hotel::Reservation
     end
 
     it "has a start date that is an instance of Ruby's Date class" do
-      @reservation_0_nominal.start_date.must_be_instance_of DateTime
+      @reservation_0_nominal_6n.start_date.must_be_instance_of DateTime
     end
 
     it "has an end date that is an instance of Ruby's Date class" do
-      @reservation_0_nominal.end_date.must_be_instance_of DateTime
+      @reservation_0_nominal_6n.end_date.must_be_instance_of DateTime
     end
 
     it "Raises an error if the start date comes before the date of instantiation" do
@@ -32,31 +32,51 @@ describe "Reservation class" do
 
     it "raises an error if the end date is not at least one day after the end date" do
       late_start = '3rd May 3075'
-      early_end = '1st May 3075'
+      early_end = '3rd May 3075'
       proc{ Hotel::Reservation.new(late_start, early_end) }.must_raise StandardError
     end
   end
 
-  # describe "calculate_cumulative_price" do
-  #   it "returns a float rounded to two decimal places" do
-  #     @reservation_0_nominal.calculate_cumulative_price.must_be_kind_of Float
-  #     @reservation_0_nominal.calculate_cumulative_price.to_s.must_match /^\d+\.\d{2}$/
-  #   end
-  #
-  #   it "accurately returns the product of the room's per-day price and the length (in days) of the reservation" do
-  #     @reservation_0_nominal.calculate_cumulative_price.must_be_within_delta 600.00, 0.003
-  #   end
-  # end
-
   describe "assign_id" do
     it "creates an eight-digit id" do
-      @reservation_0_nominal.id.to_s.must_match /^\d{8}$/
-      @reservation_3_id_check.id.to_s.must_match /^\d{8}$/
+      @reservation_0_nominal_6n.id.to_s.must_match /^\d{8}$/
+      @reservation_3_35n.id.to_s.must_match /^\d{8}$/
     end
 
     it "generates an ID number that is one higher than the next-highest ID number" do
-      @reservation_2_id_check.id.to_i.must_equal 1 + @reservation_1_id_check.id.to_i
-      @reservation_3_id_check.id.to_i.must_equal 2 + @reservation_1_id_check.id.to_i
+      @reservation_2_2n.id.to_i.must_equal 1 + @reservation_1_1n.id.to_i
+      @reservation_3_35n.id.to_i.must_equal 2 + @reservation_1_1n.id.to_i
+    end
+  end
+
+  describe "calculate_total_nights" do
+    before do
+      @r_0_nominal_nights = @reservation_0_nominal_6n.calculate_total_nights
+    end
+    it "returns an integer" do
+      @r_0_nominal_nights.must_be_kind_of Integer
+    end
+    it "correctly calculates the length of a nominal stay" do
+      @r_0_nominal_nights.must_equal 6
+    end
+    it "correctly calculates the length of a stay that begins in one year and ends in another" do
+      @reservation_3_35n.calculate_total_nights.must_equal 35
+    end
+    it "correctly calculates teh length of a stay that is one night long" do
+      @reservation_1_1n.calculate_total_nights.must_equal 1
+    end
+  end
+
+  describe "calculate_reservation_price" do
+    it "returns a float rounded to two decimal places" do
+      @reservation_0_nominal_6n.calculate_reservation_price.must_be_kind_of Float
+      @reservation_0_nominal_6n.calculate_reservation_price.to_s.must_match /^\d+\.\d{2}$/
+    end
+
+    it "accurately returns the product of the room's per-day price and the length (in days) of the reservation" do
+      @reservation_0_nominal_6n.calculate_reservation_price.must_be_within_delta 600.00, 0.003
+      @reservation_1_1n.calculate_reservation_price.must_be_within_delta 100.00, 0.003
+      @reservation_3_35n.calculate_reservation_price.must_be_within_delta 3500.00, 0.003
     end
   end
 end
